7b301bac400c672d6f52b6d199fa183129b1112f
ThamerAyachi <thamerjak3@gmail.com>
2024-04-24 11:24:00 +0100

    try fix task 2

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -2,13 +2,12 @@ class Password {
   String password = "";
 
   bool isValid() {
-    return (password.length >= 8) &&
-        (password.length >= 16) &&
+    return ((password.length >= 8) && (password.length <= 16)) &&
         this.password.contains(new RegExp(r"[A-Z]")) &&
         this.password.contains(new RegExp(r"[a-z]")) &&
         this.password.contains(new RegExp(r"[0-9]"));
   }
 
   @override
-  String toString() => "Your password is: ${this.password}";
+  String toString() => "Your Password is: ${this.password}";
 }
